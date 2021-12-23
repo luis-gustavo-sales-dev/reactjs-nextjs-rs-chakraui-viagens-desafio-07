b941d8c935c04d8eedea100ba13594b95ffd288f
Luis Gustavo <luisgssf@gmail.com>
2021-12-23 13:50:44 -0300

    Initial configuration for chakra-UI

---
+++ styles/theme.ts
@@ -0,0 +1,23 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+    colors: {
+        background: '#1D1D1D',
+        highlight: '#FFBA08',
+        heading: '#47585B',
+        text: '#47585B',
+        info: '#999999'
+    },
+    fonts: {
+        heading: 'Poppins',
+        body: 'Poppins',
+    },
+    styles: {
+        global: {
+            body: {
+                bg: 'background',
+                color: 'text'
+            }
+        }
+    }
+})
